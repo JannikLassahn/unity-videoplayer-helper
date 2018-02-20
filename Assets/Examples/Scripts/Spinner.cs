c8087e487289b9d25d54ed11d8280af756998fd4
Jannik Lassahn <jannik.lassahn@gmx.de>
2018-02-20 02:02:20 +0100

    Add Vimeo sample

---
+++ Assets/Examples/Scripts/Spinner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public RectTransform Progress;
+    public float RotationSpeed = 100f;
+
+    private void Update()
+    {
+        Progress.Rotate(0f, 0f, RotationSpeed * Time.deltaTime);
+    }
+}
